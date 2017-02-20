d1a6c93ce4e09c007a41f96ccb2557e88cb5ed5f
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2017-02-20 19:40:41 +0000

    Implemented main basic views of the User (signed in/out/up) and the links from the navbar to those views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+// After 3 seconds the alerts are vanishing
+  setTimeout(
+    function() {
+      $('.alert').fadeOut(1000);
+  }, 3000);
+})
+
