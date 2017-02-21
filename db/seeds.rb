bac8cc87cf5f47c1ce5c8ba5562195edb7cb7437
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2017-02-21 22:24:17 +0000

    Prepared a seed that gives a basis for working and testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..10
+
+  email = "user"+ i.to_s + "@example.com"
+  full_name = Faker::GameOfThrones.character.split(" ")
+  first_name = full_name[0]
+  last_name = full_name[1]
+  a_user = User.create(email: email, first_name: first_name, last_name: last_name, password: "lisbon2017")
+  rand(0..5).times do
+    pony_name = Faker::Cat.name
+    pony_description = "This pony is very " + %w(gentle active strong fast beautiful smart).sample
+    pony_description += " and is loved by most people that meet it. On top of that, " + Faker::Hipster.sentence
+    location = rand(1..150).to_s + " " + Faker::GameOfThrones.house + " " + %w(street road alley pass).sample + " , " + Faker::GameOfThrones.city
+    a_pony = Pony.create(name: pony_name, description: pony_description, location: location, user: a_user)
+    a_pony.photo_url = "http://loremflickr.com/320/240/pony"
+    rand(0..10).times do
+      start_date = Faker::Date.forward(30)
+      end_date = start_date + rand(30..200)
+      daily_rate = rand(100..200)
+      PonyOffer.create(start_date: start_date, end_date: end_date, daily_rate: daily_rate, pony: a_pony, most_recent: true)
+    end
+  end
+end
