1697fc44c980ffb98f821c5cb24b3607bb86e6cc
Carlos Silva <cucmaluco@gmail.com>
2017-02-21 16:18:47 +0000

    Index and Show working for PonyOffers

--- app/controllers/pony_offers_controller.rb
+++ app/controllers/pony_offers_controller.rb
@@ -1,11 +1,22 @@
 class PonyOffersController < ApplicationController
+
+  before_action :set_ponyoffer, only: [:show, :destroy]
+
+
+
   def index
+    @ponyoffer = PonyOffer.all
   end
 
+  # Done
   def show
   end
 
+
+
   def new
+    @poney = Poney.find(params[:poney_id])
+    @ponyoffer = PonyOffer.new
   end
 
   def create
@@ -13,4 +24,21 @@ class PonyOffersController < ApplicationController
 
   def destroy
   end
+
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ponyoffer
+      @ponyoffer = PonyOffer.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def ponyoffer_params
+      params.require(:ponyoffer).permit(:pony_id, :start_date, :end_date, :daily_rate)
+    end
+
+
+
 end
